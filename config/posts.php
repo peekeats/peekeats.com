741b8eb5d5511001fae4c3a70eec12a245ad5f04
T Rock <terence.chia@gmail.com>
2026-01-01 11:21:57 +1100

    post menu option config

--- config/posts.php
+++ config/posts.php
@@ -2,7 +2,8 @@
 
 return [
     // Enable or disable the posts feature. Controlled via env `POSTS_ENABLED`.
-    'enabled' => env('POSTS_ENABLED', true),
+    // Allow either `POST_ENABLED` (legacy/typo) or `POSTS_ENABLED` to control feature.
+    'enabled' => env('POST_ENABLED', env('POSTS_ENABLED', true)),
 
     // Model used for posts. Set via env `POSTS_MODEL` to override.
     // Example in .env: POSTS_MODEL=App\\Models\\WpPost
