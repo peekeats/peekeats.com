95aa563a59852d6bd69e1a72c329015e5c927e97
T Rock <terence.chia@gmail.com>
2026-01-04 08:20:39 +1100

    favicon for games

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'GD Login')</title>
-    <link rel="icon" type="image/png" href="{{ asset('glitchdata_logo1.png') }}">
+    <?php $currentTheme = config('frontpage.theme', 'default'); ?>
+    <link rel="icon" href="{{ $currentTheme === 'games' ? asset('assets/games/favicon.svg') : asset('glitchdata_logo1.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
